246c2ae0d353c196f5ca0f56de1cd62c7fd9f189
Kazik Pogoda <morisil@xemantic.com>
2024-09-30 13:19:38 +0200

    message creation test

---
+++ src/commonTest/kotlin/message/MessagesTest.kt
@@ -0,0 +1,59 @@
+package com.xemantic.anthropic.message
+
+import com.xemantic.anthropic.anthropicJsonConfigurer
+import io.kotest.assertions.json.shouldEqualJson
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.intellij.lang.annotations.Language
+import org.junit.Test
+
+/**
+ * Tests the JSON serialization format of created Anthropic API messages.
+ */
+class MessagesTest {
+
+  /**
+   * A pretty JSON printing for testing.
+   */
+  private val json = Json {
+    anthropicJsonConfigurer()
+    prettyPrint = true
+  }
+
+  @Test
+  fun shouldCreateTheSimplestMessageRequest() {
+    // given
+    val request = MessageRequest(
+      defaultModel = "claude-3-opus-20240229"
+    ) {
+      +Message {
+        +"Hey Claude!?"
+      }
+    }
+
+    // when
+    val json = json.encodeToString(request)
+
+    // then
+    @Language("JSON")
+    val expected = """
+      {
+        "model": "claude-3-opus-20240229",
+        "messages": [
+          {
+            "role": "user",
+            "content": [
+              {
+                "type": "text",
+                "text": "Hey Claude!?"
+              }
+            ]
+          }
+        ],
+        "max_tokens": 1024
+      }
+    """.trimIndent()
+    json shouldEqualJson expected
+  }
+
+}
