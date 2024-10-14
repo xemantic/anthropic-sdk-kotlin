782dc71390dd3275b4e56ea57c918322c148999a
Kazik Pogoda <morisil@xemantic.com>
2024-10-14 18:06:55 +0200

    JvmAnthropic update

--- src/jvmMain/kotlin/JvmAnthropic.kt
+++ src/jvmMain/kotlin/JvmAnthropic.kt
@@ -1,7 +1,40 @@
 package com.xemantic.anthropic
 
+import com.xemantic.anthropic.message.MessageRequest
+import com.xemantic.anthropic.message.MessageResponse
+import kotlinx.coroutines.runBlocking
+import java.util.function.Consumer
+
 actual val envApiKey: String?
   get() = System.getenv("ANTHROPIC_API_KEY")
 
 actual val missingApiKeyMessage: String
   get() = "apiKey is missing, it has to be provided as a parameter or as an ANTHROPIC_API_KEY environment variable."
+
+// a very early version of Java only SDK, adapting Kotlin idioms and coroutines
+// it might change a lot in the future
+class JavaAnthropic {
+
+  companion object {
+
+    @JvmStatic
+    fun create(): Anthropic = Anthropic()
+
+    @JvmStatic
+    fun create(
+      configurer: Consumer<Anthropic.Config>
+    ): Anthropic {
+      return Anthropic { configurer.accept(this) }
+    }
+
+  }
+
+}
+
+fun Anthropic.createMessage(
+  builder: Consumer<MessageRequest.Builder>
+): MessageResponse = runBlocking {
+  messages.create {
+    builder.accept(this)
+  }
+}
