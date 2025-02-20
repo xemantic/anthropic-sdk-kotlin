713bbc20d5409c75a45a12d75b93dddbaad3774a
Kazik Pogoda <morisil@xemantic.com>
2025-02-20 09:59:47 +0100

    updated TextEditorTool and TestEditorToolTest

---
+++ src/commonMain/kotlin/tool/computer/TextEditorTool.kt
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2024-2025 Kazimierz Pogoda / Xemantic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xemantic.ai.anthropic.tool.computer
+
+import com.xemantic.ai.anthropic.tool.BuiltInTool
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.serializer
+
+@Serializable
+@SerialName("str_replace_editor")
+class TextEditorTool private constructor() : BuiltInTool(
+    name = "str_replace_editor",
+    type = "text_editor_20250124"
+) {
+
+    @Suppress("unused")
+    enum class Command {
+
+        @SerialName("view")
+        VIEW,
+
+        @SerialName("create")
+        CREATE,
+
+        @SerialName("str_replace")
+        STR_REPLACE,
+
+        @SerialName("insert")
+        INSERT,
+
+        @SerialName("undo_edit")
+        UNDO_EDIT
+    }
+
+    @Serializable
+    class Input private constructor(
+        val command: Command,
+        @SerialName("file_text")
+        val fileText: String?,
+        @SerialName("insert_line")
+        val insertLine: Int?,
+        @SerialName("new_str")
+        val newStr: String?,
+        @SerialName("old_str")
+        val oldStr: String?,
+        @SerialName("path")
+        val path: String,
+        @SerialName("view_range")
+        val viewRange: List<Int>?
+    ) {
+
+        class Builder {
+
+            var command: Command? = null
+            var fileText: String? = null
+            var insertLine: Int? = null
+            var newStr: String? = null
+            var oldStr: String? = null
+            var path: String? = null
+            val viewRange: List<Int>? = null
+
+            fun build(): Input = Input(
+                requireNotNull(command) { "command cannot be null" },
+                fileText,
+                insertLine,
+                newStr,
+                oldStr,
+                requireNotNull(path) { "path cannot be null" },
+                viewRange
+            )
+
+        }
+
+    }
+
+    class Builder {
+
+        fun build(): TextEditorTool = TextEditorTool()
+
+    }
+
+    companion object {
+
+        fun Input(
+            block: Input.Builder.() -> Unit
+        ): Input = Input.Builder().also(block).build()
+
+    }
+
+}
+
+fun TextEditorTool(
+    builder: TextEditorTool.Builder.() -> Unit = {},
+    run: suspend TextEditorTool.Input.() -> Unit
+): TextEditorTool = TextEditorTool.Builder().also(builder).build().apply {
+    inputSerializer = serializer<TextEditorTool.Input>()
+    runner = { input -> run(input as TextEditorTool.Input) }
+}
---
+++ src/commonTest/kotlin/tool/computer/TextEditorToolTest.kt
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2025 Kazimierz Pogoda / Xemantic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xemantic.ai.anthropic.tool.computer
+
+import com.xemantic.ai.anthropic.Anthropic
+import com.xemantic.ai.anthropic.json.anthropicJson
+import com.xemantic.ai.anthropic.message.Message
+import com.xemantic.ai.anthropic.message.StopReason
+import com.xemantic.ai.anthropic.tool.Tool
+import com.xemantic.kotlin.test.be
+import com.xemantic.kotlin.test.have
+import com.xemantic.kotlin.test.should
+import io.kotest.assertions.json.shouldEqualJson
+import kotlinx.coroutines.test.runTest
+import kotlinx.serialization.encodeToString
+import org.junit.Test
+
+class TextEditorToolTest {
+
+    @Test
+    fun `should use TextEditorTool`() = runTest {
+        // given
+        var actualInput: TextEditorTool.Input? = null
+        val tool = TextEditorTool { actualInput = this }
+        val anthropic = Anthropic()
+
+        // when
+        val response = anthropic.messages.create {
+            +Message { +"Look at the /tmp/foo.txt file" }
+            tools = listOf(tool)
+        }
+
+        // then
+        response should {
+            have(stopReason == StopReason.TOOL_USE)
+        }
+
+        // when
+        response.useTools()
+
+        // then
+        actualInput should {
+            have(command == TextEditorTool.Command.VIEW)
+            have(path == "/tmp/foo.txt")
+        }
+    }
+
+    @Test
+    fun `should serialize TextEditorTool`() {
+        anthropicJson.encodeToString(
+            TextEditorTool {}
+        ) shouldEqualJson /* language=json */ """
+           {
+             "name": "str_replace_editor",
+             "type": "text_editor_20250124"
+           }
+        """
+    }
+
+    @Test
+    fun `should deserialize TextEditorTool`() {
+        // given
+        /* language=json */
+        val json =  """
+           {
+             "name": "str_replace_editor",
+             "type": "text_editor_20250124"
+           }
+        """
+
+        // when
+        val tool = anthropicJson.decodeFromString<Tool>(json)
+
+        // then
+        tool should {
+            have(name == "str_replace_editor")
+            be<TextEditorTool>()
+            have(type == "text_editor_20250124")
+        }
+    }
+
+    @Test
+    fun `should return JSON for TextEditorTool toString`() {
+        TextEditorTool {}.toString() shouldEqualJson /* language=json */ """
+           {
+             "name": "str_replace_editor",
+             "type": "text_editor_20250124"
+           }
+        """
+    }
+
+    @Test
+    fun `should deserialize TextEditorTool Input`() {
+        anthropicJson.decodeFromString<TextEditorTool.Input>(
+            /* language=json */
+            """
+                {
+                  "command": "view",
+                  "path": "/tmp/foo.txt"
+                }
+            """
+        ) should {
+            have(command == TextEditorTool.Command.VIEW)
+            have(path == "/tmp/foo.txt")
+            have(fileText == null)
+            have(insertLine == null)
+            have(newStr == null)
+            have(oldStr == null)
+            have(viewRange == null)
+        }
+    }
+
+    @Test
+    fun `should serialize TextEditorTool Input with command`() {
+        anthropicJson.encodeToString(TextEditorTool.Input {
+            command = TextEditorTool.Command.VIEW
+            path = "/tmp/foo.txt"
+        }) shouldEqualJson /* language=json */ """
+            {
+              "command": "view",
+              "path": "/tmp/foo.txt"
+            }
+        """.trimIndent()
+    }
+
+
+}
